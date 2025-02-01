c315b66d97f5d5f9558ae29bbd340c73a4cd3099
Pietroo03 <pietro.ponte.03@gmail.com>
2025-02-01 13:46:11 +0100

    add components

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,23 @@
+import Header from "./Components/Header"
+import About from "./Components/About"
+import Skills from "./Components/Skills"
+import Projects from "./Components/Projects"
+import Footer from "./Components/Footer"
 
 function App() {
 
   return (
     <>
+
+      <main>
+
+        <Header />
+        <About />
+        <Skills />
+        <Projects />
+        <Footer />
+
+      </main>
 
     </>
   )
