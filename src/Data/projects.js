e3e8cca3b26b00d57a92e663dce0757a37f64ef0
Pietroo03 <pietro.ponte.03@gmail.com>
2025-02-02 18:30:26 +0100

    add data

--- src/Data/projects.js
+++ src/Data/projects.js
@@ -2,7 +2,13 @@ const projects = [
     {
         title: 'BoolB&B',
         description: 'Una Web di visualizzazione appartamenti messi a disposizione da proprietari registrati. La Web App permette di registrare il proprio appartamento previa registrazione.',
-
-
+        image: '',
+    },
+    {
+        title: 'Boolfix',
+        description: 'Una Web App per visualizzare dettagli di film e serie TV. La Web App permette di ricercare un film o una serie Tv tramite una barra di ricerca.',
+        image: '',
     }
 ]
+
+export default projects
---
+++ src/Data/skills.js
@@ -0,0 +1,3 @@
+const skills = ['HTML5', 'CSS', 'JavaScript', 'Node.js', 'Express', 'React', 'Bootstrap', 'NPM', 'Git']
+
+export default skills
