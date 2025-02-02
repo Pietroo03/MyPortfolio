0ef16add206c35c9bec11c72abd00f5dbd6acace
Pietroo03 <pietro.ponte.03@gmail.com>
2025-02-02 12:33:47 +0100

    add data

---
+++ src/Data/projects.js
@@ -0,0 +1,8 @@
+const projects = [
+    {
+        title: 'BoolB&B',
+        description: 'Una Web di visualizzazione appartamenti messi a disposizione da proprietari registrati. La Web App permette di registrare il proprio appartamento previa registrazione.',
+
+
+    }
+]
